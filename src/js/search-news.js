ed64ad4eca3620c3b84ee23990c042357f129f70
Denys <denys.drhn@gmail.com>
2023-03-24 22:53:24 +0200

    update searh-news.js

--- src/js/search-news.js
+++ src/js/search-news.js
@@ -1,4 +1,5 @@
 import NytService from './nyt-api';
+<<<<<<< Updated upstream
 
 const nytService = new NytService();
 
@@ -7,6 +8,18 @@ refs = {
 		searchForm: document.querySelector('.header-form '),
 	
 		newsList: document.querySelector('.news__list'),
+=======
+import defaultUrl from '../images/default-images/def-img-tabl.png';
+import iconSprite from '../images/icons.svg';
+
+
+const nytService = new NytService();
+
+const refs = {
+	searchBtn: document.querySelector('.header-form__btn--inner'),
+	searchForm: document.querySelector('.header-form '),
+	newsList: document.querySelector('.news__list'),
+>>>>>>> Stashed changes
 };
 
 refs.searchForm.addEventListener('submit', onSearchFormSubmit);
@@ -27,6 +40,7 @@ async function onSearchFormSubmit(event) {
 		const data = await nytService.fetchByQuery();
 
 		if (data.meta.hits === 0) {
+<<<<<<< Updated upstream
 		console.log("Sorry, there are no news matching your search query. Please try again.");
 		};
 
@@ -38,10 +52,25 @@ async function onSearchFormSubmit(event) {
 //жодна категорія зі списку категорій не повинна бути активною "
 } catch (error) {
 console.log(error)
+=======
+			console.log("Sorry, there are no news matching your search query. Please try again.");
+			return null;
+		};
+
+		const news = data.docs;
+		// console.log(news);
+		clearNewsMarkup();
+		appendNewsMarkup(news);
+		// У разі успішного відпрацювання запиту, 
+		//жодна категорія зі списку категорій не повинна бути активною "
+	} catch (error) {
+		console.log(error)
+>>>>>>> Stashed changes
 	};
 };
 
 function appendNewsMarkup(news) {
+<<<<<<< Updated upstream
 	
 	const markup = news.map((article) => `<li class="news__card-item">
  	<div class="article">
@@ -71,11 +100,70 @@ function appendNewsMarkup(news) {
  		</a>
  	</div>
  </li>`).join("");
+=======
+
+	const markup = news.map(e => {
+
+		const { abstract, headline: { main: headlineMain }, pub_date, web_url, section_name, media } = e;
+		console.log(e);
+		let scripture = abstract;
+		if (scripture.length > 112) {
+			scripture = scripture.slice(0, 112) + '...';
+		}
+
+		let imageUrl = '';
+		let imageCaption = '';
+		if (!media || media.length < 1) {
+			imageUrl = defaultUrl;
+		} else {
+			media.map(e => {
+				imageUrl = e['media-metadata'][2].url;
+				if (e.caption === '') {
+					imageCaption = 'default picture';
+				} else {
+					imageCaption = e.caption;
+				}
+			});
+		}
+
+		return `<li class="news__card-item">
+    <div class="article">
+      <div class="article__image_wrapper">        
+        <img
+          src="${imageUrl}"
+          alt="${imageCaption}"
+        />        
+        <div class="article__category-label">${section_name}</div>
+        <button class="article__btn" type="button">
+          Add to favorite
+          <svg class="article__heart-icon" width="16" height="16">
+            <use href="${iconSprite + '#heart-like'}"></use>
+          </svg>
+        </button>
+      </div>
+  
+      <div class="article__content">
+        <h2 class="article__header">${headlineMain}</h2>
+        <p class="article__subheader">${scripture}</p>
+        <div class="article__footer">
+          <p class="article__date">${pub_date}</p>
+          <a href="${web_url}" class="article__readmore-link link-unstyled">Read more</a>
+        </div>
+      </div>
+    </div>
+  </li>`;
+	}).join("");
+>>>>>>> Stashed changes
 
 	refs.newsList.insertAdjacentHTML('beforeend', markup);
 };
 
 function clearNewsMarkup() {
+<<<<<<< Updated upstream
 refs.newsList.innerHTML = "";
 };
 
+=======
+	refs.newsList.innerHTML = "";
+};
+>>>>>>> Stashed changes
