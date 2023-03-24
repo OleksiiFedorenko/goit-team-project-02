16ea7688fadf5ddc3a046336c9e7bd71a27c9295
Vadim Yatskul <vadimajj@gmail.com>
2023-03-24 11:32:16 +0200

    grid 1 var

--- src/js/test-nyt-api.js
+++ src/js/test-nyt-api.js
@@ -10,3 +10,70 @@ const nytService = new NytService();
 
 nytService.query = 'Ukraine';
 nytService.fetchByQuery();
+
+/////    //////    ///////
+const newsList = document.querySelector('.news__list');
+
+fetchAndAddArticles();
+
+function articleMarkup(article) {
+  const { abstract, lead_paragraph, web_url, pub_date } = article;
+  return `<li class="articles__card-item">
+  <div class="article">
+    <a class="article__link link-unstyled" href="${web_url}">
+      <div class="article__image_wrapper">
+        <picture>
+          <source
+            srcset="/src/images/test-image.jpg", /src/images/test-image.jpg, 2x"
+            width="395"
+            height="395"
+          />
+          <img
+            src="/src/images/test-image.jpg"
+            alt="News article"
+            width="395"
+            height="395"
+          />
+        </picture>
+        <div class="article__category-label">News category</div>
+        <button class="article__btn" type="button">
+          Add to favorite
+          <svg class="article__heart-icon" width="16" height="16">
+            <use href="/src/images/icons.svg#heart-like"></use>
+          </svg>
+        </button>
+      </div>
+
+      <div class="article__content">
+        <h2 class="article__header">
+          8 tips for passing an online interview that will help you
+                      get a job
+        </h2>
+        <p class="article__subheader">
+          Before you start looking for a job, it is useful to
+                      familiarize yourself with the job prospects offered by
+                      these...
+        </p>
+        <div class="article__footer">
+          <p class="article__date">${pub_date}</p>
+          <a href="${web_url}" class="article__readmore-link link-unstyled">Read more</a>
+        </div>
+      </div>
+    </a>
+  </div>
+</li>`;
+}
+function makeNewsFeed(articles) {
+  //console.log(articles);
+  return articles.map(articleMarkup).join('');
+}
+function appendArticles(articles) {
+  newsList.insertAdjacentHTML('beforeend', makeNewsFeed(articles));
+}
+
+function fetchAndAddArticles() {
+  nytService.query = 'Ukraine';
+  nytService.fetchByQuery().then(response => {
+    appendArticles(({ articles } = response.docs));
+  });
+}
