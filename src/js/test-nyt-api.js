20879069267106e1edd736563031de8fa3917e9c
Oleksii Fedorenko <fedorenkolesha@gmail.com>
2023-03-24 16:11:16 +0200

    Adjust a bit

--- src/js/test-nyt-api.js
+++ src/js/test-nyt-api.js
@@ -1,4 +1,6 @@
 import NytService from './nyt-api';
+import testImg from '../images/test-image.jpg';
+import iconSprite from '../images/icons.svg';
 const nytService = new NytService();
 
 // nytService.fetchMostPopular();
@@ -20,38 +22,35 @@ function articleMarkup(article) {
   const { abstract, lead_paragraph, web_url, pub_date } = article;
   return `<li class="news__card-item">
   <div class="article">
-    <a class="article__link link-unstyled" href="${web_url}">
-      <div class="article__image_wrapper">        
-          <img
-            src="/src/images/test-image.jpg"
-            alt="News article"            
-            height="395"
-          />        
-        <div class="article__category-label">News category</div>
-        <button class="article__btn" type="button">
-          Add to favorite
-          <svg class="article__heart-icon" width="16" height="16">
-            <use href="/src/images/icons.svg#heart-like"></use>
-          </svg>
-        </button>
-      </div>
+    <div class="article__image_wrapper">        
+      <img
+        src="${testImg}"
+        alt="News article"
+      />        
+      <div class="article__category-label">News category</div>
+      <button class="article__btn" type="button">
+        Add to favorite
+        <svg class="article__heart-icon" width="16" height="16">
+          <use href="${iconSprite + '#heart-like'}"></use>
+        </svg>
+      </button>
+    </div>
 
-      <div class="article__content">
-        <h2 class="article__header">
-          8 tips for passing an online interview that will help you
-                      get a job
-        </h2>
-        <p class="article__subheader">
-          Before you start looking for a job, it is useful to
-                      familiarize yourself with the job prospects offered by
-                      these...
-        </p>
-        <div class="article__footer">
-          <p class="article__date">${pub_date}</p>
-          <a href="${web_url}" class="article__readmore-link link-unstyled">Read more</a>
-        </div>
+    <div class="article__content">
+      <h2 class="article__header">
+        8 tips for passing an online interview that will help you
+                    get a job
+      </h2>
+      <p class="article__subheader">
+        Before you start looking for a job, it is useful to
+                    familiarize yourself with the job prospects offered by
+                    these...
+      </p>
+      <div class="article__footer">
+        <p class="article__date">${pub_date}</p>
+        <a href="${web_url}" class="article__readmore-link link-unstyled">Read more</a>
       </div>
-    </a>
+    </div>
   </div>
 </li>`;
 }
