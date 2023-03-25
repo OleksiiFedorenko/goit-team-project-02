cd926fa1e6f96776ab5a90fa4603dec78f1fe959
Elizabeth Pereriadkina <el.pereryadkina@gmail.com>
2023-03-25 05:36:29 +0100

    Update favorite-add-btn.js

--- src/js/favorite-add-btn.js
+++ src/js/favorite-add-btn.js
@@ -3,34 +3,33 @@
     
     addFavoriteBtn.addEventListener('click', onFavoriteBtnClick);
 
-function onFavoriteBtnClick(e) {
-    if (!e.target.classList.contains('target')) {
-     return;
+    function onFavoriteBtnClick(e) {
+        if (!e.target.classList.contains('target') && e.target.nodeName !== 'YA-TR-SPAN' && e.target.nodeName !== 'use') {
+        return;
     }
 
-    console.log(e.target);
 
     switch (e.target.nodeName) {
+         case 'SPAN':
+            addClassToFavoriteBtn(e.target.parentNode);
+            break;
+        
         case 'BUTTON':
             addClassToFavoriteBtn(e.target);
-        break;
-
-        case 'SPAN':
-            addClassToFavoriteBtn(e.target.parentNode);
-        break;
+            break;
         
         default:
-            addClassToFavoriteBtn(e.target.parentNode);
-        break;
+            addClassToFavoriteBtn(e.target.parentNode.parentNode);
+            break;
     }
 }
 
-function addClassToFavoriteBtn(name) {
-  if (name.classList.contains('article__btn-favorite')) {
-    name.classList.remove('article__btn-favorite');
-    name.firstElementChild.textContent = 'Add to favorite';
-  } else {
-      name.classList.add('article__btn-favorite');
-      name.firstElementChild.textContent = 'Remove from favorite';
-  }
-}
+    function addClassToFavoriteBtn(name) {
+    if (name.classList.contains('article__btn-favorite')) {
+        name.classList.remove('article__btn-favorite');
+        name.firstElementChild.textContent = 'Add to favorite';
+    } else {
+        name.classList.add('article__btn-favorite');
+        name.firstElementChild.textContent = 'Remove from favorite';
+    }
+    }
