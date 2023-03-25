45aac263eac40e469a08b7de88100bd141d45cf7
Elizabeth Pereriadkina <el.pereryadkina@gmail.com>
2023-03-25 06:15:44 +0100

    Update favorite-add-btn.js

--- src/js/favorite-add-btn.js
+++ src/js/favorite-add-btn.js
@@ -1,35 +1,36 @@
 
-    const newsList = document.querySelector('.news__list');
+const newsList = document.querySelector('.news__list');
+
+newsList.addEventListener('click', onFavoriteBtnClick);
+
+function onFavoriteBtnClick(e) {
+        if (!e.target.classList.contains('target') && e.target.nodeName !== 'YA-TR-SPAN' && e.target.nodeName !== 'use') {
+        return;
+    }
+
+
+    switch (e.target.nodeName) {
+        case 'SPAN':
+            addClassToFavoriteBtn(e.target.parentNode);
+            break;
+        
+        case 'BUTTON':
+            addClassToFavoriteBtn(e.target);
+            break;
+        
+        default:
+            addClassToFavoriteBtn(e.target.parentNode.parentNode);
+            break;
+    }
+}
+
+function addClassToFavoriteBtn(name) {
+    if (name.classList.contains('article__btn-favorite')) {
+        name.classList.remove('article__btn-favorite');
+        name.firstElementChild.textContent = 'Add to favorite';
+    } else {
+        name.classList.add('article__btn-favorite');
+        name.firstElementChild.textContent = 'Remove from favorite';
+    }
+}
     
-    newsList.addEventListener('click', onFavoriteBtnClick);
-
-    function onFavoriteBtnClick(e) {
-            if (!e.target.classList.contains('target') && e.target.nodeName !== 'YA-TR-SPAN' && e.target.nodeName !== 'use') {
-            return;
-        }
-
-
-        switch (e.target.nodeName) {
-            case 'SPAN':
-                addClassToFavoriteBtn(e.target.parentNode);
-                break;
-            
-            case 'BUTTON':
-                addClassToFavoriteBtn(e.target);
-                break;
-            
-            default:
-                addClassToFavoriteBtn(e.target.parentNode.parentNode);
-                break;
-        }
-    }
-
-    function addClassToFavoriteBtn(name) {
-        if (name.classList.contains('article__btn-favorite')) {
-            name.classList.remove('article__btn-favorite');
-            name.firstElementChild.textContent = 'Add to favorite';
-        } else {
-            name.classList.add('article__btn-favorite');
-            name.firstElementChild.textContent = 'Remove from favorite';
-        }
-    }
