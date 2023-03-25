57dfaf59c81b6d6f4716b01e4fd990c39a80639b
Elizabeth Pereriadkina <el.pereryadkina@gmail.com>
2023-03-25 09:22:20 +0100

    first vertion of adding data to local storage

--- src/js/favorite-add-btn.js
+++ src/js/favorite-add-btn.js
@@ -2,6 +2,9 @@
 const newsList = document.querySelector('.news__list');
 
 newsList.addEventListener('click', onFavoriteBtnClick);
+const STORAGE_KEY = 'favoriteNews';
+let idFavoriteCard = 0;
+const favoriteNewsData = {};
 
 function onFavoriteBtnClick(e) {
         if (!e.target.classList.contains('target') && e.target.nodeName !== 'YA-TR-SPAN' && e.target.nodeName !== 'use') {
@@ -22,6 +25,7 @@ function onFavoriteBtnClick(e) {
             addClassToFavoriteBtn(e.target.parentNode.parentNode);
             break;
     }
+
 }
 
 function addClassToFavoriteBtn(name) {
@@ -31,6 +35,25 @@ function addClassToFavoriteBtn(name) {
     } else {
         name.classList.add('article__btn-favorite');
         name.firstElementChild.textContent = 'Remove from favorite';
+        idFavoriteCard += 1;
+        addItemToLocalStorage(name, idFavoriteCard);
     }
 }
     
+function addItemToLocalStorage(name, id) {
+    try {
+        favoriteNewsData[id] = {
+            articleHeader: name.parentNode.nextElementSibling.firstElementChild.firstElementChild.textContent,
+            imgSrc: name.parentNode.firstElementChild.src,
+            imgAlt: name.parentNode.firstElementChild.alt,
+            description: name.parentNode.nextElementSibling.firstElementChild.nextElementSibling.textContent,
+            date: name.parentNode.nextElementSibling.lastElementChild.firstElementChild.textContent,
+            linkReadMore: name.parentNode.nextElementSibling.lastElementChild.lastElementChild.href,
+        }
+
+        const serializedState = JSON.stringify(favoriteNewsData);
+        localStorage.setItem(STORAGE_KEY, serializedState);
+    } catch (error) {
+    console.error("Set state error: ", error.message);
+    }
+}
