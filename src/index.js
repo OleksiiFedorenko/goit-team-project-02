63b8424ef773de7c139c5d0696b2973f93eef73d
Vadim Yatskul <vadimajj@gmail.com>
2023-03-25 16:56:10 +0200

    favoretes page update

--- src/index.js
+++ src/index.js
@@ -2,3 +2,4 @@ import './js/header';
 import './js/filters';
 import './js/start-news';
 // import './js/test-nyt-api';
+import './js/favorite-news';
--- src/js/favorite-news.js
+++ src/js/favorite-news.js
@@ -1,10 +1,98 @@
-import defaultUrl from '../images/default-images/def-img-tabl.png';
 import iconSprite from '../images/icons.svg';
+const FAVORITES_KEY = 'favotiteNews';
 
-function getavorites() {
-  const savedFavorites = localStorage.getItem('favorite');
-  if (savedFavorites) {
-    const { abstract, title, published_date, url, section, media } =
-      JSON.parse(savedFavorites);
-  }
+const favoriteEl = document.querySelector('.favorite__list');
+
+const savedFavorites = localStorage.getItem(FAVORITES_KEY);
+const parsedFavorites = Object.values(JSON.parse(savedFavorites));
+
+console.log(parsedFavorites);
+
+appendArticles();
+
+function appendArticles(articles) {
+  favoriteEl.insertAdjacentHTML('beforeend', makeNewsGallery(articles));
 }
+
+function makeNewsGallery() {
+  return parsedFavorites.map(articleMarkup).join('');
+}
+function articleMarkup(articles) {
+  const {
+    articleHeader: title,
+    date: published_date,
+    imgSrc: imageUrl,
+    imgAlt: imageCaption,
+    description: scripture,
+    linkReadMore: url,
+  } = articles;
+  return `<li class="news__card-item">
+    <div class="article">
+      <div class="article__image_wrapper">        
+        <img
+          src="${imageUrl}"
+          alt="${imageCaption}"
+        />        
+        <div class="article__category-label">Article section</div>
+        <button class="article__btn" type="button">
+          Add to favorite
+          <svg class="article__heart-icon" width="16" height="16">
+            <use href="${iconSprite + '#heart-like'}"></use>
+          </svg>
+        </button>
+      </div>
+  
+      <div class="article__content">
+        <h2 class="article__header">${title}</h2>
+        <p class="article__subheader">${scripture}</p>
+        <div class="article__footer">
+          <p class="article__date">${published_date}</p>
+          <a href="${url}" class="article__readmore-link link-unstyled">Read more</a>
+        </div>
+      </div>
+    </div>
+  </li>`;
+}
+
+// Код нижче тільки для тестів (запису тестового об'єкту в localstorage)
+
+function testLocalStorage() {
+  const testArticle = {
+    1: {
+      articleHeader: 'For Sale: Mansions in Los Angeles at Bargain Prices',
+      date: '1985',
+      description:
+        'With an emphasis on solutions to the environmental crisis, Ecologues creates a much-needed space to debate and educate.',
+      imgSrc:
+        'https://static01.nyt.com/images/2023/03/24/multimedia/24trump-rhetoric-01-bflw/24trump-rhetoric-01-bflw-mediumThreeByTwo440.jpg',
+      imgAlt: 'Some text',
+      linkReadMore: '#',
+    },
+    2: {
+      articleHeader:
+        'Trump, Escalating Attacks, Raises Specter of Violence if He Is Charged',
+      date: '2001',
+      description:
+        'With an emphasis on solutions to the environmental crisis, Ecologues creates a much-needed space to debate and educate.',
+      imgSrc:
+        'https://ichef.bbci.co.uk/news/976/cpsprodpb/22DC/production/_129142980_c34727dc9528ad3e21061d4cb12b948471561908178_213_3312_18631000x563.jpg.webp',
+      imgAlt: 'Some text',
+      linkReadMore: '#',
+    },
+    3: {
+      articleHeader:
+        'Expelling Rahul Gandhi From Parliament, Modi Allies Thwart a Top Rival',
+      date: '2023',
+      description:
+        'With an emphasis on solutions to the environmental crisis, Ecologues creates a much-needed space to debate and educate.',
+      imgSrc:
+        'https://ichef.bbci.co.uk/news/976/cpsprodpb/FCD7/production/_102672746_mediaitem92725505.jpg.webp',
+      imgAlt: 'Some text',
+      linkReadMore: '#',
+    },
+  };
+
+  localStorage.setItem(FAVORITES_KEY, JSON.stringify(testArticle));
+}
+
+testLocalStorage();
