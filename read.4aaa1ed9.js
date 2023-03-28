ce279c07dbc964e0f0c75eac5d4b995426bcccd4
OleksiiFedorenko <113383083+OleksiiFedorenko@users.noreply.github.com>
2023-03-28 11:14:57 +0000

    Deploying to gh-pages from @ OleksiiFedorenko/goit-team-project-02@52c2678f7824cc831f736666b9dc2eaf45b3f745 🚀

--- read.4aaa1ed9.js
+++
@@ -1,2 +0,0 @@
-!function(){var e="undefined"!=typeof globalThis?globalThis:"undefined"!=typeof self?self:"undefined"!=typeof window?window:"undefined"!=typeof global?global:{},n={},r={},o=e.parcelRequired7c6;null==o&&((o=function(e){if(e in n)return n[e].exports;if(e in r){var o=r[e];delete r[e];var i={id:e,exports:{}};return n[e]=i,o.call(i.exports,i,i.exports),i.exports}var d=new Error("Cannot find module '"+e+"'");throw d.code="MODULE_NOT_FOUND",d}).register=function(e,n){r[e]=n},e.parcelRequired7c6=o),o("i8Q71"),o("gUYrH"),o("6IdhT")}();
-//# sourceMappingURL=read.4aaa1ed9.js.map
