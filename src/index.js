f51a28ed811024306eb7b17176a5fd99869892b5
Oleksii Fedorenko <fedorenkolesha@gmail.com>
2023-03-28 14:03:04 +0300

    Adjust a bit

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import './js/start-news';
 import './js/news-by-cat';
 import './js/search-news';
 import './js/pagination';
-import './js/accordion';
 
+// import './js/accordion';
 // import './js/read-news';
 // import './js/test-nyt-api';
