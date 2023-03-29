8457ccd0903832fcc00f84b83f4166ad91f6a43b
Alexander-Solonar <112549802+Alexander-Solonar@users.noreply.github.com>
2023-03-29 16:03:29 +0300

    fix-padding

--- src/js/make-accordion-card.js
+++ src/js/make-accordion-card.js
@@ -1,6 +1,6 @@
 export function makeAccordionCard(data) {
   const { date, contentMarkup } = data;
-  return `<div class="accordion-card">
+  return `<li class="accordion-card">
   <button  aria-label="Unfold Articles Based on Date" class="accordion-card__head">${date}
       <svg viewBox="0 0 32 32" class="accordion-card__icon">
           <path d="m4.576 22.4-3.509-3.247L16 5.334l14.933 13.819-3.509 3.247L16 11.851 4.576 22.4z" />
@@ -9,5 +9,5 @@ export function makeAccordionCard(data) {
   <div class="accordion-card__body">
       <div class="accordion-card__content">${contentMarkup}</div>
   </div>
-</div>`;
+</li>`;
 }
